3b31ea4fb2488ab56db801418fe711bda06fb813
Dimitar Minchev <98884203+DimitarM45@users.noreply.github.com>
2025-03-22 10:34:01 +0200

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,11 @@
+.image-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+.image-container img {
+    width: 50px;
+    height: auto;
+    margin-bottom: 10px; 
